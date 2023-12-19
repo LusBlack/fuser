485980771dfa3df8ab863dfb9731ccf4e5eae598
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-19 03:47:38 +0100

    import errors fixed

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"net/http"
+
+	"mongo-golang/controllers"
 )
 
 func main() {
 
 	r := httprouter.New()
 	uc := controllers.NewUserController(getSession)
-	r.GET("/user/:id",uc.GetUser)
-	r.POST("/user",uc.CreateUser )
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
-	http.ListenAndServe("localhost:9000",r)
+	http.ListenAndServe("localhost:9000", r)
 
 }
 
 func getSession() *mgo.Session {
 
 	s, err := mgo.Dial("mongodb://loclahost:27107")
-		if err != nil {
-			panic(err)
-		}
-		return s
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
